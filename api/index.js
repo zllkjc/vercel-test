778d34d6be8115fc169537376f11525668359e8a
zllkjc <68810266+zllkjc@users.noreply.github.com>
2021-02-22 19:02:45 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,6 +7,9 @@ app.get('/api', (req, res) => {
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
 })
+app.get('/hello', (req, res) => {
+  res.end(`Hello`)
+})
 
 app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params
